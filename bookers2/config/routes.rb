369ea67f4451f2b5debe682feac31924a91aacad
rui-21 <ec2-user@ip-172-31-6-108.ec2.internal>
2021-04-20 13:11:43 +0000

    [Add]Views Model

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  
+  resources :books, only: [:index, :show, :edit, :update, :create, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
