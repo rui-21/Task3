9b946b32cd4ec83c0a9aea1eb3f47b6b2183fa1c
rui-21 <ec2-user@ip-172-31-6-108.ec2.internal>
2021-04-21 12:17:58 +0000

    [Update]UserPage

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @new_book = Book.new
+    @users = User.all
+    @user = User.find(current_user.id)
   end
 
   def show
+    @new_book = Book.new
     @user = User.find(params[:id])
+    @books = @user.books.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image ,:introduction )
+  end
+  
 end
