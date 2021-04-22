ace7e7792cf3c87272a33a36aa26ff605e397c79
rui-21 <reisu6235@gmail.com>
2021-04-22 03:50:46 +0000

    [Add]success_message

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @new_book = Book.new
     @books = Book.all
     @user = User.find(current_user.id)
@@ -12,10 +13,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(book.id), notice: 'You have created book successfully.'
+    else
+      @new_book = Book.new
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render :index
+    end
   end
 
   def edit
