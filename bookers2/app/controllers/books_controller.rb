538caf0b1eb327d828eeb9d6ec7d96e6489a58b2
rui-21 <reisu6235@gmail.com>
2021-04-22 06:40:05 +0000

    [Fix]:opinion=>:body

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(book.id), notice: 'You have created book successfully.'
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
       @new_book = Book.new
       @books = Book.all
@@ -29,8 +29,9 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.user == current_user
       render :edit
-    end
+    else
       redirect_to books_path
+    end
   end
 
   def update
@@ -50,6 +51,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 end
---
+++ bookers2/db/migrate/20210422061528_remove_opinion_from_book.rb
@@ -0,0 +1,5 @@
+class RemoveOpinionFromBook < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :books, :opinion, :text
+  end
+end
